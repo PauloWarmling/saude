e76dafc829d2476ef76e9fd54d27249db9ed8f32
PauloWarmling <paulowarmling12@gmail.com>
2024-04-04 21:35:27 -0300

    versao 0.0.1 projeto saude

--- app/Http/Controllers/ImcController.php
+++ app/Http/Controllers/ImcController.php
@@ -4,10 +4,48 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ImcController extends Controller
-{
+class ImcController extends Controller {
+
     public function index() {
+        $this->store();
+    }
+
+    public function store(Request $request) {
+
+        $request->validate([
+            'altura' => 'required|numeric|min:0',
+            'peso' => 'required|numeric|min:0',
+        ]);
+
+        $altura = $request->altura;
+        $peso = $request->peso;
+    
+        $imc = $this->calcularImc($altura, $peso);
+    
+        if ($imc === -1) {
+            $erro = 'Erro';
+            return view('imc', $erro);
+        }
+    
+        return view('imc', $imc);
+    }
+    
+    /**
+     * Calcula o IMC com base nos parâmetros fornecidos.
+     *
+     * @param  float  $altura
+     * @param  float  $peso
+     * @return float
+     */
+    public function calcularImc($altura, $peso) {
+        // Verificando se altura e peso são válidos
+        if ($altura <= 0 || $peso <= 0) {
+            return -1; // Retornar um valor negativo para indicar erro
+        }
+
+        // Calculando o IMC
+        $imc = $peso / ($altura * $altura);
         
-        return view('imc');
-     }
+        return $imc;
+    }
 }
--- resources/views/imc.blade.php
+++ resources/views/imc.blade.php
@@ -1,16 +1,20 @@
 @extends('layout.app')
 <div>
     @section('content')
-    <form action="" method='post'>
+    <form action="{{ route('imc.store') }}" method='post'>
 
         <label for="idade">Idade</label>
-        <input type="date" id='idade'>
+        <input type="date" id='idade' name='idade'>
 
         <label for="altura">Altura</label>
-        <input type="text" id='altura'>
+        <input type="text" id='altura' name='altura'>
 
         <label for="peso">Peso</label>
-        <input type="text" id='peso'>
+        <input type="text" id='peso' name='peso'>
+
+        <input type="submit" value='Enviar'>
     </form>
+
+    <p>IMC: {{$imc}}</p>
     @endsection
 </div>
